f79da96e0f9344cb67c49a850de4d96a98871f78
Ryou <81555178+Ryosuke1218@users.noreply.github.com>
2026-02-09 18:44:01 +0900

    typo

--- dc4client/__init__.py
+++ dc4client/__init__.py
@@ -1,6 +1,6 @@
 
-from dcclient.dc_client import DCClient
-from dcclient.match_maker_client import MatchMakerClient
+from dc4client.dc_client import DCClient
+from dc4client.match_maker_client import MatchMakerClient
 
 __all__ = [
 	"DCClient",
